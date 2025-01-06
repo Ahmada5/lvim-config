d4c7678b901a8ea7510ccf164ae5ea78f70f2fc3
ahmada5 <achmad.soleh.ahmada@gmail.com>
2025-01-06 13:20:35 +0700

    updt - make the c charp into project

--- testingformat/cicharp/cisharp.cs
+++ testingformat/cicharp/cisharp.cs
@@ -1,1 +1,14 @@
 //testing files for the c#, will use it in the context of unity
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mtfk!");    
+        }
+    }
+}
+
